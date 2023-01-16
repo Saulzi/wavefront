7174a0bb470b678557b8fdb06cacf2626a14f704
Tom <mrsaulzi@gmail.com>
2023-01-16 21:34:47 +0000

    #1 Created Application

---
+++ Wavefront/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Wavefront
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+        }
+    }
+}
